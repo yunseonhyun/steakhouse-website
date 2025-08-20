b65259bada50a3ea025c4c7aec788cfa127350c5
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-08-20 12:01:29 +0900

    js 수정본

--- js/login.js
+++ js/login.js
@@ -2,3 +2,7 @@ document.getElementById("통합회원가입").addEventListener("click", function
   event.preventDefault();
   window.location.href = "register.html";
 });
+
+document.getElementById("close").addEventListener("click", function () {
+  window.close();
+});
